08af6eff0d0396c1a8bb9338a255c2f700bfd8f1
ashui <ashui@ashuihouse.cn>
2017-06-08 13:29:48 +0800

    3.1
    3.1

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your cade here
+  var s=0;
+  for(var i=0;i<str.length;i++)
+  {
+     s=s+parseInt(str[i]);
+  }
+  
 };
